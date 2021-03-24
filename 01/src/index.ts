a9d2ead8805ca771e039a65fdf92e83b36d16f0f
Sven Laschinski <sven@laschinski.net>
2021-03-24 10:32:43 +0100

    add first exercise

--- 01/src/index.ts
+++ 01/src/index.ts
@@ -1,1 +1,6 @@
-console.log("It's working!");
+/**
+ * Create a variable with a value of 10.
+ * Create an easy comparison that outputs something to the console if the value is 10.
+ * Do not output anything if the value is not 10.
+ * Try adjusting the variable to see if everything works correctly.
+ */
