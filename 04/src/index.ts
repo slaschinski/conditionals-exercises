4ec246fe889df52e307e88cf81b3636bb649b95e
Sven Laschinski <sven@laschinski.net>
2021-03-24 10:43:26 +0100

    add forth exercise

---
+++ 04/src/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Get the first parameter after the script name of your terminal.
+ * Compare this parameter to '-r' and '-f' and output something like:
+ * 'You typed -r'
+ * If the parameter is neither '-r' nor '-f', output a help text showing
+ * which parameters are accepted.
+ */
