286918407e689dd1c2b43c899e9ae01e0c35eca9
Sven Laschinski <sven@laschinski.net>
2021-03-24 10:40:45 +0100

    add two more exercises

---
+++ 02/src/index.ts
@@ -0,0 +1,6 @@
+/**
+ * Create a variable with a value of 10.
+ * Create an easy comparison that outputs something to the console if the value is 10.
+ * Output something else if the value is not 10.
+ * Try adjusting the variable to see if everything works correctly.
+ */
---
+++ 03/src/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Output the first parameter after the script name of your terminal.
+ * Use 'process.env' for this.
+ * For example:
+ * node build/index.js Hello
+ * It should output 'Hello' to the console.
+ */
