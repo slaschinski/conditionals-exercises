2e45b8e27b4ec48061b57890c776bb8d110c6555
Sven Laschinski <sven@laschinski.net>
2021-03-24 10:50:24 +0100

    correct typo

--- 03/src/index.ts
+++ 03/src/index.ts
@@ -1,6 +1,6 @@
 /**
  * Output the first parameter after the script name of your terminal.
- * Use 'process.env' for this.
+ * Use 'process.argv' for this.
  * For example:
  * node build/index.js Hello
  * It should output 'Hello' to the console.
